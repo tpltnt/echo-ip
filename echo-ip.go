9137b48cbc92dde490828c4a95f0d314e36cdef8
tpltnt <tpltnt.github@dropcut.net>
2013-09-21 22:23:11 +0200

    listening socket added

--- echo-ip.go
+++ echo-ip.go
@@ -1,4 +1,15 @@
 package main
 
+import(
+        "log"
+        "net"
+)
+
 func main() {
+     // open socket
+     listensocket, err := net.Listen("tcp", ":1256")
+     // log error & close
+     if err != nil {
+        log.Fatal(err);
+     }
 }
